4adf485d033c6ddd29d0478e504b4e2a12b9d619
Napawan Srisuksawad <t.o.ohms@gmail.com>
2025-10-27 02:15:21 +1100

    Clean up remove redundant

--- utils/xml.go
+++ utils/xml.go
@@ -1,3 +1,6 @@
+/*
+Copyright © 2025 Napawan Srisuksawad <napawan.srisuksawad@gmail.com>
+*/
 package utils
 
 import (
@@ -6,11 +9,19 @@ import (
 	"os"
 )
 
+type MyXmlElement struct {
+	XMLName xml.Name `xml:"url"`
+	URL     string   `xml:"loc"`
+}
+
 func WriteToXML(links []Link, outfile string) {
 	enc := xml.NewEncoder(os.Stdout)
+	defer enc.Close()
 	enc.Indent("  ", "    ")
 	for _, link := range links {
-		if err := enc.Encode(link); err != nil {
+		xml := MyXmlElement{URL: link.Href}
+		err := enc.Encode(xml)
+		if err != nil {
 			fmt.Printf("error: %v\n", err)
 		}
 	}
